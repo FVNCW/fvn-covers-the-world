22be24ed8ec2b9ebaae44c1c0b0ffb1cdd8b7068
陨落基围虾 <3161880837@qq.com>
2026-08-25 21:38:38 +0800

    chore: 移除打印env变量和端口号的调试日志
    删除了开发调试阶段遗留的控制台打印语句，清理代码冗余输出

--- src/index.ts
+++ src/index.ts
@@ -2,8 +2,6 @@ import { app } from "./engine/app";
 import { env } from "./engine/env-load";
 import "./engine/route";
 
-console.log(env);
-
 app.listen(Number(env.FCTW_PORT), () => {
     console.log(env.FCTW_PORT);
 });
