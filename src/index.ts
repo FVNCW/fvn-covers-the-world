2b0fdc36a6587ff1df19f5850a2cdc602ab7d56d
陨落基围虾 <3161880837@qq.com>
2026-08-26 15:29:28 +0800

    refactor: 调整导入语句顺序并统一缩进格式
    调整了模块导入的位置，将app和route的导入移到合适位置，同时统一了console.log的缩进风格，提升代码可读性

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,13 @@
-import { app } from "./engine/app";
 import { env } from "./engine/env-load";
-import "./engine/route";
 import cliOptions from "./engine/cli";
 
 if (cliOptions.env) {
-    console.log(env);
+	console.log(env);
 }
 
+import { app } from "./engine/app";
+import "./engine/route";
+
 app.listen(Number(env.FCTW_PORT), () => {
-    console.log(env.FCTW_PORT);
+	console.log(env.FCTW_PORT);
 });
